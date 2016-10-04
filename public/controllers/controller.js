64f0d137d6dbe5032b7ad93d5478bc56cff6a94c
Manojkumar <manojingalagi199@gmail.com>
2016-10-04 20:00:04 +0530

    Dummy data to test table

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,27 @@
 var myApp = angular.module('myApp',[]);
 
-function AppCtrl() {
-  console.log("Hello world from controller")
+function AppCtrl($scope) {
+        console.log("Hello world from controller");
+
+        //Dummy data for test
+        person1 = {
+            name: 'Manoj',
+            email: 'manojingalagi199@gmail.com',
+            number: '8867813367'
+        };
+
+        person2 = {
+            name: 'Manju',
+            email: 'manjuingalagi052@gmail.com',
+            number: '9035782155'
+        };
+
+        person3 = {
+            name: 'Suraj',
+            email: 'Suraj@gmail.com',
+            number: '8451126451'
+        };
+
+        var contactlist = [person1,person2,person3];
+        $scope.contactlist = contactlist;
 }
