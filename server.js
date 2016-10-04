8b593e111afd0ee789d8cdfd1ae1e9a8d768cc2e
Manojkumar <manojingalagi199@gmail.com>
2016-10-04 17:24:54 +0530

    AngularJS connection test

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express =  require('express');
 var app =  express();
 
-app.get('/',  function (req, res) {
-  res.send("Hello from server.js")
 
-});
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log("Server running on port 3000");
