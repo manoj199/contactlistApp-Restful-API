f32cfd5cd750fdd51d8b5844ec529a90f04ac2d3
Manojkumar <manojingalagi199@gmail.com>
2016-10-04 20:58:42 +0530

    Retrieve data from server - Method 2

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,27 +1,13 @@
 var myApp = angular.module('myApp',[]);
 
-function AppCtrl($scope) {
+function AppCtrl($scope, $http) {
         console.log("Hello world from controller");
 
+        $http.get('/contactlist').success(function(response){
+          console.log("I got the data i requested");
+          $scope.contactlist = response;
+        });
         //Dummy data for test
-        person1 = {
-            name: 'Manoj',
-            email: 'manojingalagi199@gmail.com',
-            number: '8867813367'
-        };
 
-        person2 = {
-            name: 'Manju',
-            email: 'manjuingalagi052@gmail.com',
-            number: '9035782155'
-        };
 
-        person3 = {
-            name: 'Suraj',
-            email: 'Suraj@gmail.com',
-            number: '8451126451'
-        };
-
-        var contactlist = [person1,person2,person3];
-        $scope.contactlist = contactlist;
 }
--- server.js
+++ server.js
@@ -4,6 +4,30 @@ var app =  express();
 
 
 app.use(express.static(__dirname + "/public"));
+app.get('/contactlist', function(req, res){
+  console.log("I received a get request")
+
+  person1 = {
+      name: 'Manoj',
+      email: 'manojingalagi199@gmail.com',
+      number: '8867813367'
+  };
+
+  person2 = {
+      name: 'Manju',
+      email: 'manjuingalagi052@gmail.com',
+      number: '9035782155'
+  };
+
+  person3 = {
+      name: 'Suraj',
+      email: 'Suraj@gmail.com',
+      number: '8451126451'
+  };
+
+  var contactlist = [person1,person2,person3];
+  res.json(contactlist);
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
