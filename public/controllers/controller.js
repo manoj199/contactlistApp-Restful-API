1ceb185b00da373f64fa1579810e1b1fe2531f4f
Manojkumar <manojingalagi199@gmail.com>
2016-10-05 12:10:03 +0530

    Add data to view automatically after clicking  Add Contact

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,15 +3,23 @@ var myApp = angular.module('myApp',[]);
 function AppCtrl($scope, $http) {
         console.log("Hello world from controller");
 
-        $http.get('/contactlist').success(function(response){
-          console.log("I got the data i requested");
-          $scope.contactlist = response;
+        var refresh  = function() {
+            $http.get('/contactlist').success(function(response){
+                  console.log("I got the data i requested");
+                  $scope.contactlist = response;
+                  $scope.contact ="";
         });
-      $scope.addContact = function(){
-        console.log($scope.contact);
-        $http.post('/contactlist', $scope.contact).success(function(response){
-          console.log(response);
-        });
+
+      };
+
+      refresh();
+        
+          $scope.addContact = function(){
+                console.log($scope.contact);
+                $http.post('/contactlist', $scope.contact).success(function(response){
+                      console.log(response);
+                      refresh();
+          });
       };
 
 
